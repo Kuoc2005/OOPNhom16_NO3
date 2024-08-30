92e9aad720d857c13e0e6067311bde7b73917ac6
NguyenDuyQuang2005 <151505180+NguyenDuyQuang2005@users.noreply.github.com>
2024-08-30 10:51:51 +0700

    Create thoigian.java

---
+++ thoigian.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+import java.time.LocalDateTime;
+
+public class Thời gian {
+    public void hienThiThoiGianHienTai() {
+        System.out.println("Thời gian hiện tại: " + LocalDateTime.now());
+    }
+}
