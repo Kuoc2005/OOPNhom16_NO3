7539996768f763446892806ca6698e534201d392
NguyenDuyQuang2005 <151505180+NguyenDuyQuang2005@users.noreply.github.com>
2024-08-30 11:44:26 +0700

    Create style.css

---
+++ baitap/style.css
@@ -0,0 +1,28 @@
+package com.mycompany.app;
+
+public class App {
+    public static void main(String[] args) {
+        // Sử dụng lớp Sách
+        Sách sach = new Sách("Lập trình Java", "Nguyễn Văn A");
+        sach.hienThiThongTin();
+
+        // Sử dụng lớp Thời gian
+        Thời gian thoiGian = new Thời gian();
+        thoiGian.hienThiThoiGianHienTai();
+
+        // Sử dụng lớp Đệ quy
+        Đệ quy deQuy = new Đệ quy();
+        int ketQua = deQuy.tinhGiaiThua(5);
+        System.out.println("Giai thừa của 5 là: " + ketQua);
+
+        // Sử dụng lớp NNCollection
+        NNCollection nnCollection = new NNCollection();
+        nnCollection.themNN("Java");
+        nnCollection.themNN("Python");
+        nnCollection.hienThiDanhSachNN();
+
+        // Sử dụng lớp TênSố
+        TênSố tenSo = new TênSố(2);
+        System.out.println("Tên của số 2 là: " + tenSo.tenSo());
+    }
+}
