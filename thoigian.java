5cd2a46c0b616ec38cbee2ed7c329863c638f9cd
NguyenDuyQuang2005 <151505180+NguyenDuyQuang2005@users.noreply.github.com>
2024-08-30 11:46:32 +0700

    Delete thoigian.java

--- thoigian.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.app;
-
-import java.time.LocalDateTime;
-
-public class Thời gian {
-    public void hienThiThoiGianHienTai() {
-        System.out.println("Thời gian hiện tại: " + LocalDateTime.now());
-    }
-}
